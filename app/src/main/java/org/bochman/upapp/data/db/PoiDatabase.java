8435d0122017f89fc25ec02c7e45674a97ee9e3a
OrenBochman <OrenBochman@gmail.com>
2019-11-04 11:51:13 +0200

    added migration to support changes in Poi object

--- app/src/main/java/org/bochman/upapp/data/db/PoiDatabase.java
+++ app/src/main/java/org/bochman/upapp/data/db/PoiDatabase.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import org.bochman.upapp.data.dao.PoiDao;
 import org.bochman.upapp.data.enteties.Poi;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 // bump version number if your schema changes
-@Database(entities = {Poi.class}, version = 1)
+@Database(entities = {Poi.class}, version = 2)
 public abstract class PoiDatabase extends RoomDatabase {
 
     // Declare your data access objects as abstract
@@ -18,6 +21,14 @@ public abstract class PoiDatabase extends RoomDatabase {
 
     // Database name to be used
     public static final String NAME = "PoiDatabase";
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE poi ADD COLUMN photoUri TEXT");
+            database.execSQL("ALTER TABLE poi ADD COLUMN photoUri TEXT");
+        }
+    };
 
     //There must be only one database instance!
     private static volatile PoiDatabase INSTANCE;
@@ -29,6 +40,8 @@ public abstract class PoiDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             PoiDatabase.class, NAME)
+                        //    .addMigrations(MIGRATION_1_2)
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
