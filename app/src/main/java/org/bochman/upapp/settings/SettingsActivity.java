e2abc7af3af64e0abb89c9919c1e678016677961
OrenBochman <OrenBochman@gmail.com>
2019-11-04 11:22:09 +0200

    connected to from activity favourites and from masterPoiList
    added back menu item
    and menu handler for it.

--- app/src/main/java/org/bochman/upapp/settings/SettingsActivity.java
+++ app/src/main/java/org/bochman/upapp/settings/SettingsActivity.java
@@ -1,12 +1,17 @@
 package org.bochman.upapp.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.preference.PreferenceFragmentCompat;
 
 import org.bochman.upapp.R;
+import org.bochman.upapp.masterDetail.POIMasterActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -29,5 +34,50 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+    }
+
+    /**
+     * handle menu events.
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //using the same menu as master activity.
+        getMenuInflater().inflate(R.menu.places_list_activity_menu, menu);
+        MenuItem preferencesItem = menu.findItem(R.id.action_preferences);
+        return true;
+    }
+
+    /**
+     * Menu click handler.
+     *
+     * Navigate back to parent activity.
+     *
+     * @param item - the menu item being accessed
+     * @return if handled
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_preferences:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpTo(this, new Intent(this, POIMasterActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
