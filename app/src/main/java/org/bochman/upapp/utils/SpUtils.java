fae5f1fad8394c78ad5b258672f6a3e4b30e9347
OrenBochman <OrenBochman@gmail.com>
2019-11-07 11:44:09 +0200

    added getters for new settings types and cleaned up

--- app/src/main/java/org/bochman/upapp/utils/SpUtils.java
+++ app/src/main/java/org/bochman/upapp/utils/SpUtils.java
@@ -1,48 +1,63 @@
 package org.bochman.upapp.utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * Shared Preferences Utilities.
+ *
+ * TODO: Inject this as a singleton using dagger and depend on the app for context.
+ */
 public class SpUtils {
-    static final String NAME = "History";
 
-    static final String LAT = "lat";
-    static final String LNG = "lng";
-    static final String QUERY = "query";
-    static final String IS_METRIC = "isMetric";
+    // keys of shared preferences:
 
-    static void setFloat(Double x, String key, Context ctx) {
+    static final String NAME = "History";               // key for sp file
+
+    static final String LAT = "lat";                    // set in PoiMasterActivity
+    static final String LNG = "lng";                    // set in PoiMasterActivity
+    static final String QUERY = "query";                // set in PoiMasterActivity
+    static final String IS_METRIC = "units";            // set in root_values
+    static final String RADIUS = "radius_values";       // set in root_values
+    static final String MAP_TYPE = "map_type";          // set in root_values
+    static final String PLACE_TYPE = "place_type";      // set in root_values
+
+    // some defaults:
+
+    private static final float LNG_DEFAULT= 34.8149f;
+    private static final float LAT_DEFAULT= 32.0668f;
+    private static final int RADIUS_DEFAULT= 1000;
+
+    private static final String MAP_TYPE_DEFAULT= "normal";
+
+    public static int getRadius(Context ctx) {
         android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME, Context.MODE_PRIVATE);
-        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putFloat(key, x.floatValue());
-        editor.apply();
+        return sharedPreferences.getInt(RADIUS, RADIUS_DEFAULT);
     }
 
-    static void setInt(int x, String key, Context ctx) {
+    public static Set<String> getMapType(Context ctx) {
         android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME, Context.MODE_PRIVATE);
-        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(key, x);
-        editor.apply();
+        return sharedPreferences.getStringSet(PLACE_TYPE, new HashSet<String>());
     }
 
-    static void setString(String x, String key, Context ctx) {
+    public static String getPlaceType(Context ctx) {
         android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME, Context.MODE_PRIVATE);
-        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(key, x);
-        editor.apply();
+        return sharedPreferences.getString(MAP_TYPE, MAP_TYPE_DEFAULT);
     }
-
     public static int getIsMetric(Context ctx) {
         android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getInt(IS_METRIC, 1);
-
     }
 
     public static void setIsMetric(int isMetric, Context ctx) {
         setInt(isMetric,IS_METRIC,ctx);
     }
+
     public static String getLastSearch(Context ctx) {
         android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME, Context.MODE_PRIVATE);
 
@@ -56,23 +71,49 @@ public class SpUtils {
     }
 
     public static double getLat(Context ctx) {
-        return ctx.getSharedPreferences("lat", Context.MODE_PRIVATE).
-                getFloat("lat", 32.0668f);
+        return ctx.getSharedPreferences(LAT, Context.MODE_PRIVATE).
+                getFloat(LAT, LAT_DEFAULT);
     }
 
     public static void setLat(Double lat, Context ctx) {
+
         setFloat(lat, LAT, ctx);
     }
 
-
-    public static double getLng(Context ctx) {
-        return ctx.getSharedPreferences("lat", Context.MODE_PRIVATE).
-                getFloat("lat", 34.8149f);
-    }
-
     public static void setLng(Double lat, Context ctx) {
+
         setFloat(lat, LNG, ctx);
     }
 
+    public static double getLng(Context ctx) {
+
+        return ctx.getSharedPreferences(LNG, Context.MODE_PRIVATE).getFloat(LNG, LNG_DEFAULT);
+    }
+
+    // Internal setters per DRY ///////////////////////////////////////////////////////////////////
+
+    private static void setInt(int x, String key, Context ctx) {
+        android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME,
+                Context.MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, x);
+        editor.apply();
+    }
+
+    private static void setString(String x, String key, Context ctx) {
+        android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME,
+                Context.MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, x);
+        editor.apply();
+    }
+
+    private static void setFloat(Double x, String key, Context ctx) {
+        android.content.SharedPreferences sharedPreferences = ctx.getSharedPreferences(NAME,
+                Context.MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(key, x.floatValue());
+        editor.apply();
+    }
 
 }
