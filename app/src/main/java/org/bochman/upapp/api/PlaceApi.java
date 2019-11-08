81c146e461cdad8c5552613e2f280bf37782a922
OrenBochman <OrenBochman@gmail.com>
2019-11-08 14:41:02 +0200

    updating testing configuration for robolectric

---
+++ app/src/main/java/org/bochman/upapp/api/PlaceApi.java
@@ -0,0 +1,76 @@
+package org.bochman.upapp.api;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import org.bochman.upapp.BuildConfig;
+import org.bochman.upapp.data.enteties.Poi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Class to encapsulate the logic of the api request from the handler thread.
+ * <p>
+ * This class is added to facilitate unit testing use of the API without the
+ * the complexity introduced by the HandlerService threading.
+ */
+public class PlaceApi {
+
+    PlacesClient placesClient;
+
+    public PlaceApi(Context ctx) {
+        //init places
+        Places.initialize(ctx, BuildConfig.google_maps_key);
+        placesClient = Places.createClient(ctx);
+    }
+
+    /**
+     * for intial query
+     */
+    static final List<Place.Field> findCurrentPlaceRequestFields
+        = Arrays.asList(
+                Place.Field.ID,
+                Place.Field.NAME,
+                Place.Field.LAT_LNG,
+                Place.Field.ADDRESS,
+                Place.Field.RATING);
+
+    /**
+     * for favourites
+     */
+     static final List<Place.Field> findPlaceById
+            = Arrays.asList(
+            Place.Field.PHONE_NUMBER,
+            Place.Field.WEBSITE_URI);
+
+    CompletableFuture<List<Poi>> findCurrentLocation() {
+        return null;
+    }
+
+    CompletableFuture<Bitmap> getPhoto(Poi poi) {
+        return null;
+    }
+
+    CompletableFuture<Poi> widenPlaceData(Poi poi) {
+        return null;
+    }
+
+    int StoreResult(Poi poi) {
+        return 0;
+    }
+
+    String parseStatusCode(ApiException exception) {
+        return null;
+    }
+
+    public String getHelloWorldString() {
+        return null;
+    }
+}
---
+++ app/src/test/java/org/bochman/upapp/api/PlaceApiTest.java
@@ -0,0 +1,49 @@
+package org.bochman.upapp.api;
+
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class PlaceApiTest {
+
+    private Context context = ApplicationProvider.getApplicationContext();
+
+    private static final String FAKE_STRING = "HELLO_WORLD";
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+        // Given a Context object retrieved from Robolectric...
+        PlaceApi myObjectUnderTest = new PlaceApi(context);
+
+        // ...when the string is returned from the object under test...
+        String result = myObjectUnderTest.getHelloWorldString();
+
+        // ...then the result should be the expected one.
+        assertThat(result).isEqualTo(FAKE_STRING);
+    }
+
+    @Test
+    public void findCurrentLocation() {
+    }
+
+    @Test
+    public void getPhoto() {
+    }
+
+    @Test
+    public void widenPlaceData() {
+    }
+
+    @Test
+    public void storeResult() {
+    }
+
+    @Test
+    public void parseStatusCode() {
+    }
+}
