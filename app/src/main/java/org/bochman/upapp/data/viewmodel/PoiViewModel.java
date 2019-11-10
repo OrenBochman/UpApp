5920cd1a978dd166f6b4f9c1ee936347ab90995c
OrenBochman <OrenBochman@gmail.com>
2019-11-10 14:33:17 +0200

    added suppor for photos and poi updates

--- app/src/main/java/org/bochman/upapp/data/viewmodel/PoiViewModel.java
+++ app/src/main/java/org/bochman/upapp/data/viewmodel/PoiViewModel.java
@@ -1,7 +1,9 @@
 package org.bochman.upapp.data.viewmodel;
 
 import android.app.Application;
+import android.graphics.Bitmap;
 
+import org.bochman.upapp.data.enteties.PlacePhoto;
 import org.bochman.upapp.data.enteties.Poi;
 import org.bochman.upapp.data.repository.PoiRepository;
 
@@ -16,22 +18,35 @@ public class PoiViewModel extends AndroidViewModel {
 
     private LiveData<List<Poi>> mAllPois;
     private LiveData<List<Poi>> mAllFavs;
+    private LiveData<List<PlacePhoto>> mAllPhotos;
+    private LiveData<List<PlacePhoto>> mAllFavPhotos;
 
     public PoiViewModel(Application application) {
         super(application);
         mRepository = new PoiRepository(application);
         mAllPois = mRepository.getAllPois();
         mAllFavs = mRepository.getAllFavs();
+        mAllPhotos = mRepository.getAllPhotos();
+        mAllFavPhotos = mRepository.getAllFavPhotos();
 
+    }
+    public LiveData<Bitmap> getPhoto(String id){
+        return mRepository.getPhoto(id);
     }
 
     public LiveData<List<Poi>> getAllPois(){return mAllPois;}
 
     public LiveData<List<Poi>> getAllFavs(){return mAllFavs;}
 
+    public LiveData<List<PlacePhoto>> getAllImages() { return mAllPhotos; }
+
+    public LiveData<List<PlacePhoto>> getFavImages() { return mAllFavPhotos; }
+
     public void insert(Poi poi) { mRepository.insert(poi); }
 
     public void delete(String id) { mRepository.delete(id); }
+
+    public void update(Poi poi) { mRepository.update(poi); }
 
 
 }
