f1b50a9de80e0198ec352e0c309f6f620bd11723
OrenBochman <OrenBochman@gmail.com>
2019-11-10 14:34:51 +0200

    added insert and updates

--- app/src/main/java/org/bochman/upapp/data/repository/PoiRepository.java
+++ app/src/main/java/org/bochman/upapp/data/repository/PoiRepository.java
@@ -1,8 +1,15 @@
 package org.bochman.upapp.data.repository;
 
 import android.app.Application;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.BaseColumns;
+import android.provider.MediaStore;
 
 import org.bochman.upapp.api.HttpPoiSearchIntentService;
 import org.bochman.upapp.api.PhotoIntentService;
@@ -12,6 +19,7 @@ import org.bochman.upapp.data.db.PoiDatabase;
 import org.bochman.upapp.data.enteties.PlacePhoto;
 import org.bochman.upapp.data.enteties.Poi;
 
+import java.io.IOException;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
@@ -22,6 +30,8 @@ public class PoiRepository {
     private PlacePhotoDao mPlacePhotoDao;
     private LiveData<List<Poi>> mAllPois;
     private LiveData<List<Poi>> mAllFavs;
+    private LiveData<List<PlacePhoto>> mAllPhotos;
+    private LiveData<List<PlacePhoto>> mAllFavPhotos;
 
     public PoiRepository(Application application) {
         PoiDatabase db = PoiDatabase.getDatabase(application);
@@ -29,6 +39,16 @@ public class PoiRepository {
         mPlacePhotoDao = db.placePhotoDao();
         mAllPois = mPoiDao.getAllPlaces();
         mAllFavs = mPoiDao.getAllFavourites();
+        mAllPhotos = mPlacePhotoDao.getAllPhotos();
+        mAllFavPhotos = mPlacePhotoDao.getAllFavPhotos();
+
+    }
+    public LiveData<List<PlacePhoto>> getAllPhotos() {
+        return mAllPhotos;
+    }
+
+    public LiveData<List<PlacePhoto>> getAllFavPhotos() {
+        return mAllFavPhotos;
     }
 
     public LiveData<List<Poi>> getAllPois() {
@@ -39,12 +59,37 @@ public class PoiRepository {
         return mAllFavs;
     }
 
-    public Bitmap getPhoto(String id) {
+    public LiveData<Bitmap> getPhoto(String id) {
         return mPlacePhotoDao.getPhoto(id);
     }
 
     public void insertPhoto(PlacePhoto placePhoto) {
         new Thread(() -> mPlacePhotoDao.insertPhoto(placePhoto)).start();
+//        new Thread(() -> {
+//
+//            ContentValues values = new ContentValues();
+//            values.put(MediaStore.Images.Media.DISPLAY_NAME, placePhoto.id+".jpg");
+//            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+//            values.put(MediaStore.Images.Media.IS_PENDING, 1);
+//
+//            ContentResolver resolver = context.getContentResolver();
+//            Uri collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+//            Uri item = resolver.insert(collection, values);
+//
+//            try (ParcelFileDescriptor pfd = resolver.openFileDescriptor(item, "w", null)) {
+//                // Write data into the pending image.
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            // Now that we're finished, release the "pending" status, and allow other apps
+//            // to view the image.
+//            values.clear();
+//            values.put(MediaStore.Images.Media.IS_PENDING, 0);
+//            resolver.update(item, values, null, null);
+//
+//            // update the uri... to a content://id.jpg in the poi
+//        }).start();
     }
 
     public void delete(String id) {
@@ -59,8 +104,9 @@ public class PoiRepository {
         new Thread(() -> mPoiDao.deleteAllFavourites()).start();
     }
 
-    public void insert(Poi poi) {
-        new Thread(() -> mPoiDao.insert(poi)).start();
+    public void insert(Poi poi) { new Thread(() -> mPoiDao.insert(poi)).start(); }
 
-    }
+    public void update(Poi poi){ new Thread(() -> mPoiDao.updatePoi(poi)).start(); }
+
+
 }
