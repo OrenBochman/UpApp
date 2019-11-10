3d0e34e9ba7d2da070379efc3d9f146e9abe07eb
OrenBochman <OrenBochman@gmail.com>
2019-11-10 07:25:33 +0200

    refactored added support for getting photos

--- app/src/main/java/org/bochman/upapp/api/PoiIntentService.java
+++ app/src/main/java/org/bochman/upapp/api/PoiIntentService.java
@@ -17,6 +17,7 @@ import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
 import org.bochman.upapp.BuildConfig;
+import org.bochman.upapp.UpApp;
 import org.bochman.upapp.data.enteties.Poi;
 import org.bochman.upapp.data.repository.PoiRepository;
 import org.bochman.upapp.utils.Debug;
@@ -25,6 +26,7 @@ import org.bochman.upapp.utils.SpUtils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
 
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
@@ -37,33 +39,41 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
  */
 public class PoiIntentService extends IntentService {
 
-    public static final String QUERY = "query";
     String TAG = PoiIntentService.class.getSimpleName();
+
+    public static final String QUERY = "query";
+
+    PoiRepository mPoiRepository;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      */
     public PoiIntentService() {
         super("PoiIntentService");
+        mPoiRepository = new PoiRepository(getApplication());
+
     }
+
+    // Specify the fields to return.
+    List<Place.Field> placeFields = Arrays.asList(
+            Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.LAT_LNG,
+            Place.Field.ADDRESS,
+            Place.Field.RATING
+            //       Place.Field.PHONE_NUMBER,
+            //       Place.Field.WEBSITE_URI,
+    );
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+
         assert intent != null;
         String query = intent.getStringExtra(QUERY); // gets the query from search buttons
         float userlat = (float) SpUtils.getLat(this);
         float userlng = (float) SpUtils.getLng(this);
         Log.e(Debug.getTag(), String.format("onHandleIntent: %s %f %f ", query, userlat, userlng));
 
-        // Specify the fields to return.
-        List<Place.Field> placeFields =
-                Arrays.asList(Place.Field.ID,
-                        Place.Field.NAME,
-                        Place.Field.LAT_LNG,
-                        Place.Field.ADDRESS,
-                        //       Place.Field.PHONE_NUMBER,
-                        //       Place.Field.WEBSITE_URI,
-                        Place.Field.RATING);
         //init places
         Places.initialize(this, BuildConfig.google_maps_key);
         PlacesClient placesClient = Places.createClient(this);
@@ -71,81 +81,88 @@ public class PoiIntentService extends IntentService {
         FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
 
         // Call findCurrentPlace and handle the response (first check that the user has granted permission).
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
             placeResponse.addOnCompleteListener(task -> {
                 /// do this in another thread
                 if (task.isSuccessful()) {
-
-                    //process the response off main thread.
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            FindCurrentPlaceResponse response = task.getResult();
-                            if (response != null) {
-                                PoiRepository mPoiRepository = new PoiRepository(getApplication());
-                                mPoiRepository.deleteAllPois();
-                                for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-                                    Log.i(Debug.getTag(), String.format("Place '%s' has likelihood: %f",
-                                            placeLikelihood.getPlace().getName(),
-                                            placeLikelihood.getLikelihood()));
-
-                                    // handling nullable types in the response
-                                    Optional<LatLng> optionalLatLng = Optional.ofNullable(placeLikelihood.getPlace().getLatLng());
-                                    Optional<Double> optionalRating = Optional.ofNullable(placeLikelihood.getPlace().getRating());
-                                    Optional<Uri> optionalWebsiteUri = Optional.ofNullable(placeLikelihood.getPlace().getWebsiteUri());
-
-                                    Poi poi = new Poi(placeLikelihood.getPlace().getId(),
-                                            placeLikelihood.getPlace().getName(),
-                                            optionalLatLng.map(x -> x.latitude).orElse(0.0),
-                                            optionalLatLng.map(x -> x.longitude).orElse(0.0),
-                                            placeLikelihood.getPlace().getAddress(),
-                                            "", //        placeLikelihood.getPlace().getPhoneNumber(),
-                                            optionalWebsiteUri.map(Uri::toString).orElse(""),
-                                            optionalRating.orElse(0.0));
-
-                                    //this needs to happen off the main/ui thread
-
-                                    mPoiRepository.insert(poi);
-
-                                    //((UpApp) getApplicationContext()).getPoiDatabase().poiDao().insert(poi);
-                                    Log.i(Debug.getTag(), "Inserting into DB: " + poi.toString());
-
-                                }
-                            }
-                        }
-                    }).start();
+                    new Thread(() -> { processResult(task.getResult()); }).start();
                 } else {
-                    Exception exception = task.getException();
-                    //better error reporting to troubleshoot the api
-                    if (exception instanceof ApiException) {
-                        ApiException apiException = (ApiException) exception;
-                        String msg;
-                        switch (apiException.getStatusCode()) {
-                            case 15:
-                                msg = "Location timeout";
-                                break;
-                            case 9012:
-                                msg = "INVALID_REQUEST - place_id or field request issue";
-                                break;
-                            case 9013:
-                                msg = "NOT_FOUND - place_id no found in the db";
-                                break;
-                            case 9010:
-                                msg = "OVER_QUERY_LIMIT";
-                                break;
-                            case 9011:
-                                msg = "REQUEST_DENIED - Api missing or invalid";
-                                break;
-                        }
-                        Log.e(Debug.getTag(), "msg " + apiException.getStatusCode(), apiException);
-
-                    }
+                    processException(task.getException());
                 }
             });
 
         }
+    }
+
+    void processResult(FindCurrentPlaceResponse response) {
+
+        if (response == null) return;
+
+        mPoiRepository.deleteAllPois();
+
+        for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+            Log.i(Debug.getTag(), String.format("Place '%s' has likelihood: %f",
+                    placeLikelihood.getPlace().getName(),
+                    placeLikelihood.getLikelihood())
+            );
+
+            // handling nullable types in the response
+            Optional<LatLng> optionalLatLng = Optional.ofNullable(placeLikelihood.getPlace().getLatLng());
+            Optional<Double> optionalRating = Optional.ofNullable(placeLikelihood.getPlace().getRating());
+            Optional<Uri> optionalWebsiteUri = Optional.ofNullable(placeLikelihood.getPlace().getWebsiteUri());
+
+            //create poi
+            Poi poi = new Poi(placeLikelihood.getPlace().getId(),
+                    placeLikelihood.getPlace().getName(),
+                    optionalLatLng.map(x -> x.latitude).orElse(0.0),
+                    optionalLatLng.map(x -> x.longitude).orElse(0.0),
+                    placeLikelihood.getPlace().getAddress(),
+                    "", //        placeLikelihood.getPlace().getPhoneNumber(),
+                    optionalWebsiteUri.map(Uri::toString).orElse(""),
+                    optionalRating.orElse(0.0));
+
+            mPoiRepository.insert(poi);
+            getPhoto(poi);
+            //get the photo
+           // Intent intent = new intent();
+
+
+            //((UpApp) getApplicationContext()).getPoiDatabase().poiDao().insert(poi);
+            Log.i(Debug.getTag(), "Inserting into DB: " + poi.toString());
+
+        }
+
+    }
+
+    void processException(Exception exception) {
+        //better error reporting to troubleshoot the api
+        if (exception instanceof ApiException) {
+            ApiException apiException = (ApiException) exception;
+            String msg;
+            switch (apiException.getStatusCode()) {
+                case 15:
+                    msg = "Location timeout";
+                    break;
+                case 9012:
+                    msg = "INVALID_REQUEST - place_id or field request issue";
+                    break;
+                case 9013:
+                    msg = "NOT_FOUND - place_id no found in the db";
+                    break;
+                case 9010:
+                    msg = "OVER_QUERY_LIMIT";
+                    break;
+                case 9011:
+                    msg = "REQUEST_DENIED - Api missing or invalid";
+                    break;
+            }
+            Log.e(Debug.getTag(), "msg " + apiException.getStatusCode(), apiException);
+        }
+    }
+    private void getPhoto(Poi poi) {
+        Intent photoIntent = new Intent(this, PhotoIntentService.class);
+        photoIntent.putExtra(PhotoIntentService.PLACE_ID,poi.id);
+        startService(photoIntent);
     }
 }
