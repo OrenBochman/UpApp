ed02087453f66d0aa9832a4a368f8bddfa3f9b94
OrenBochman <OrenBochman@gmail.com>
2019-11-10 14:37:27 +0200

    added observer pattern to the photo updates

--- app/src/main/java/org/bochman/upapp/favourites/FavouritesAdapter.java
+++ app/src/main/java/org/bochman/upapp/favourites/FavouritesAdapter.java
@@ -1,5 +1,7 @@
 package org.bochman.upapp.favourites;
 
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,25 +13,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.bochman.upapp.R;
+import org.bochman.upapp.data.enteties.PlacePhoto;
 import org.bochman.upapp.data.enteties.Poi;
+import org.bochman.upapp.utils.Debug;
 import org.bochman.upapp.utils.LocationUtils;
 import org.bochman.upapp.utils.SpUtils;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesViewHolder> {
 
     // cached copy of poi data.
-
     private  List<Poi> mValues;
+    // cached copy of place photos.
+    private HashMap<String, Bitmap> mPhotos;
     // cache for the current location - updated whenever the data is updated.
     private LatLng latlng;
     // reference to the containing activity.
     private  FavouritesActivity mParentActivity;
 
     FavouritesAdapter(FavouritesActivity parent,
-                      List<Poi> items) {
+                      List<Poi> items,
+                      HashMap<String,Bitmap> mPhotos) {
         mValues = items;
+        this.mPhotos=mPhotos;
         mParentActivity = parent;
         getLatLng();
     }
@@ -54,15 +62,16 @@ public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesViewHolder
                         new LatLng(poi.lat,poi.lng),
                         SpUtils.getIsMetric(mParentActivity.getApplicationContext())==1));
 
-        if (holder.mPhoto != null)
-            if( poi.photoUri!=null && !poi.photoUri.isEmpty()) {
-                Picasso
-                        .get()
-                        .load(poi.photoUri)
-                        .into(holder.mPhoto);
-            }else{
-                holder.mPhoto.setImageDrawable(mParentActivity.getResources().getDrawable(R.drawable.ic_photo_black_24));
-            }
+        Bitmap bitmap=mPhotos.get(poi.id);
+
+        if(bitmap==null){
+            holder.mPhoto.setImageDrawable(mParentActivity.getResources().getDrawable(R.drawable.ic_photo_black_24));
+            Log.e(Debug.getTag(),String.format("Bitmap %s is  null",poi.id));
+        }else{
+            holder.mPhoto.setImageBitmap(bitmap);
+            Log.e(Debug.getTag(),String.format("Bitmap is size:  %s ",bitmap.getHeight()*bitmap.getWidth()));
+
+        }
 
         holder.ratingBar.setRating((float) poi.rating);
         //holder.ratingBar.setEnabled(false);
@@ -87,6 +96,17 @@ public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesViewHolder
      */
     public void setData(List<Poi> data){
         mValues = data;
+        getLatLng();
+        notifyDataSetChanged();
+    }
+
+    /**
+     * The method for refreshing cached photos following a search.
+     *
+     * @param mPhotos - new photo data
+     */
+    public void setPhotos(HashMap<String, Bitmap> mPhotos){
+        this.mPhotos = mPhotos;
         getLatLng();
         notifyDataSetChanged();
     }
