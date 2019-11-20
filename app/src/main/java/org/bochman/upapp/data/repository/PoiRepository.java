5c17ef294434c44e75b968d8d33481024957d8e8
OrenBochman <OrenBochman@gmail.com>
2019-11-20 15:11:11 +0200

    major changes

--- app/src/main/java/org/bochman/upapp/data/repository/PoiRepository.java
+++ app/src/main/java/org/bochman/upapp/data/repository/PoiRepository.java
@@ -1,38 +1,67 @@
 package org.bochman.upapp.data.repository;
 
 import android.app.Application;
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.ParcelFileDescriptor;
-import android.provider.BaseColumns;
-import android.provider.MediaStore;
+import android.location.Location;
+import android.os.Build;
+
+import com.github.javafaker.Bool;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.bochman.upapp.api.HttpPoiSearchIntentService;
-import org.bochman.upapp.api.PhotoIntentService;
+import org.bochman.upapp.api.PoiIntentService;
 import org.bochman.upapp.data.dao.PlacePhotoDao;
 import org.bochman.upapp.data.dao.PoiDao;
 import org.bochman.upapp.data.db.PoiDatabase;
 import org.bochman.upapp.data.enteties.PlacePhoto;
 import org.bochman.upapp.data.enteties.Poi;
+import org.bochman.upapp.utils.SpUtils;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Executor;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+// Informs Dagger that this class should be constructed only once.
+//@Singleton
+//@OpenForTesting
 
 public class PoiRepository {
 
+    private Application mApplication;
     private PoiDao mPoiDao;
     private PlacePhotoDao mPlacePhotoDao;
+
+//    private PhotoWS  mPhotoWS;
+//    private ProximitySearchWS mProximitySearchWS;
+//    private QuerySearchWS mQuerySearchWS;
+//    private AutocompleteWS mAutocompleteWS;
+
     private LiveData<List<Poi>> mAllPois;
     private LiveData<List<Poi>> mAllFavs;
     private LiveData<List<PlacePhoto>> mAllPhotos;
     private LiveData<List<PlacePhoto>> mAllFavPhotos;
+    private MutableLiveData<Boolean> mWifiOnFlag;
+    private MutableLiveData<Boolean> mChargingFlag;
+    private MutableLiveData<Optional<Location>> lastLocation;
 
+    // this is need ed to unsubscribe
+    private FusedLocationProviderClient fusedLocationClient;
+
+    //@Inject
     public PoiRepository(Application application) {
         PoiDatabase db = PoiDatabase.getDatabase(application);
         mPoiDao = db.poiDao();
@@ -41,8 +70,10 @@ public class PoiRepository {
         mAllFavs = mPoiDao.getAllFavourites();
         mAllPhotos = mPlacePhotoDao.getAllPhotos();
         mAllFavPhotos = mPlacePhotoDao.getAllFavPhotos();
+        mApplication = application;
 
     }
+
     public LiveData<List<PlacePhoto>> getAllPhotos() {
         return mAllPhotos;
     }
@@ -61,6 +92,14 @@ public class PoiRepository {
 
     public LiveData<Bitmap> getPhoto(String id) {
         return mPlacePhotoDao.getPhoto(id);
+    }
+
+
+    public int getPhotoCount(String id){
+        return mPlacePhotoDao.getPhotoCount(id);
+    }
+    public Poi getPoi(String id){
+        return mPoiDao.getById(id);
     }
 
     public void insertPhoto(PlacePhoto placePhoto) {
@@ -104,9 +143,87 @@ public class PoiRepository {
         new Thread(() -> mPoiDao.deleteAllFavourites()).start();
     }
 
-    public void insert(Poi poi) { new Thread(() -> mPoiDao.insert(poi)).start(); }
+    public void insert(Poi poi) {
+        new Thread(() -> mPoiDao.insert(poi)).start();
+    }
 
-    public void update(Poi poi){ new Thread(() -> mPoiDao.updatePoi(poi)).start(); }
+    public void update(Poi poi) {
+        new Thread(() -> mPoiDao.updatePoi(poi)).start();
+    }
 
+    /**
+     * TODO: fetch places by query + radius etc.
+     *
+     * @param query
+     */
+    public LiveData<List<Poi>> fetchPoiByQuery(String query) {
+
+        Intent intent = new Intent(mApplication, HttpPoiSearchIntentService.class);
+        intent.putExtra(HttpPoiSearchIntentService.QUERY, query);
+        mApplication.startService(intent);
+        return mAllPois;
+    }
+
+    /**
+     * fetch places by latlng + radius etc.
+     *
+     * @param latlng
+     */
+    public LiveData<List<Poi>> fetchPoiByLatLng(LatLng latlng) {
+        Intent intent = new Intent(mApplication, PoiIntentService.class);
+        intent.putExtra(PoiIntentService.QUERY, "");
+        mApplication.startService(intent);
+        return mAllPois;
+    }
+
+    public void saveSearchQuery(String query) {
+        SpUtils.setLastSearch(query, mApplication);
+    }
+
+    public String loadSearchQuery() {
+        return SpUtils.getLastSearch(mApplication);
+    }
+    
+    public LiveData<Optional<Location>> fetchLastLocation() {
+        FusedLocationProviderClient fusedLocationClient;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(mApplication);
+        fusedLocationClient.getLastLocation().addOnSuccessListener((Executor) mApplication,
+                location -> {
+                    // In some rare situations this can be null.
+                    if (location != null) {
+                        lastLocation.setValue(Optional.of(location));
+                    }
+                });
+        return lastLocation;
+    }
+
+
+
+    /**
+     * Subscribe to location updates.
+     *
+     * @return live data for the last known location.
+     */
+    public LiveData<Optional<Location>> subscribeLastKnownLocation() {
+        //create if needed
+        if(fusedLocationClient==null) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(mApplication);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    (mApplication.checkSelfPermission(ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
+                            || mApplication.checkSelfPermission(ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED)) {
+                fusedLocationClient.getLastLocation().addOnSuccessListener((Executor) mApplication,
+                        location -> lastLocation.setValue(Optional.of(location)));
+            }
+        }
+        return lastLocation;
+    }
+
+    public LiveData<Boolean> fetchWifiState() {
+        return this.mWifiOnFlag;
+    }
+
+    public LiveData<Boolean> fetchPowerState() {
+        return this.mChargingFlag;
+    }
 
 }
